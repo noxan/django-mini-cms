adc7913e53e3d8deaf0a3e5b8df76d7cbb7f33df
noxan <noxan@redmoonstudios.de>
2012-06-10 14:12:37 +0200

    write basic setuptools script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    author="noxan",
+    author_email="noxan@redmoonstudios.de",
+    name="django-mini-cms",
+    version="0.0.1",
+    description="Minimalistic django cms",
+    url="https://github.com/noxan/django-mini-cms",
+    install_requires=[
+        'Django>=1.4',
+    ],
+)
