59d69ac598f129ac307859819288dd05f12234d1
noxan <noxan@redmoonstudios.de>
2012-07-07 23:10:45 +0200

    add to unicode method to page model

--- cms/models.py
+++ cms/models.py
@@ -6,3 +6,6 @@ class Page(models.Model):
     slug = models.CharField(max_length=50, unique=True)
     content = models.TextField(blank=True)
     public = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.headline
