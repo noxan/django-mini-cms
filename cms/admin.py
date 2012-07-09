9e7379ced60ccf5bc93ee79f77868b9d34c74b64
noxan <noxan@redmoonstudios.de>
2012-07-09 15:54:33 +0200

    improve admin for page with extended list_display

--- cms/admin.py
+++ cms/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from models import Page
 
 
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    model = Page
+    list_display = ('headline', 'parent', 'slug', 'public')
+
+admin.site.register(Page, PageAdmin)
