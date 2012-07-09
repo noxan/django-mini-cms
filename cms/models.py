19f1270456e803563f4b6abf0c53b3c6ad336f5b
noxan <noxan@redmoonstudios.de>
2012-07-09 15:58:39 +0200

    add get full slug method to page model

--- cms/models.py
+++ cms/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from settings import PARENT_SEPARATOR
+
 
 class Page(models.Model):
     headline = models.CharField(max_length=255)
@@ -7,6 +9,15 @@ class Page(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, limit_choices_to={})
     content = models.TextField(blank=True)
     public = models.BooleanField(default=True)
+
+    def get_full_slug(self):
+        slug = self.slug
+        if self.parent != None:
+            parent = self.parent
+            while parent is not None:
+                slug = slug + PARENT_SEPARATOR + parent.slug
+                parent = parent.parent
+        return slug
 
     def __unicode__(self):
         return self.headline
