6a41ef97eaa99cbd06c9c94c3905015bbf7f39fd
noxan <noxan@redmoonstudios.de>
2012-07-09 15:42:32 +0200

    add parent foreign key to model

--- cms/models.py
+++ cms/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Page(models.Model):
     headline = models.CharField(max_length=255)
     slug = models.CharField(max_length=50, unique=True)
+    parent = models.ForeignKey('self', blank=True, null=True, limit_choices_to={})
     content = models.TextField(blank=True)
     public = models.BooleanField(default=True)
 
