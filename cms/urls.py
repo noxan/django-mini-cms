07c59b6ba0c08528f575932b02ae50fc6645f19c
noxan <noxan@redmoonstudios.de>
2012-07-09 15:43:22 +0200

    allow slashes in cms urls

--- cms/urls.py
+++ cms/urls.py
@@ -4,5 +4,5 @@ from views import PageDetailView
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[a-zA-Z0-9]+)/$', PageDetailView.as_view(), name='page'),
+    url(r'^(?P<slug>[a-zA-Z0-9/]+)/$', PageDetailView.as_view(), name='page'),
 )
